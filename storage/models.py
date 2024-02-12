c00768a78ccd2780425ba72c1379783492a4f2ea
AibekYrysbekov <yrysbekovayba@gmail.com>
2024-02-12 22:05:31 +0600

    T3-62: creat model category

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='storage/image', blank=True, null=True)
+    sluq = models.SlugField()
+
+    def __str__(self):
+        return self.name
