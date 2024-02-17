50c34e6e9b47ac6d3769870ee7283d7153ab38dc
AibekYrysbekov <yrysbekovayba@gmail.com>
2024-02-17 16:14:58 +0600

    T3-9:added menu app

--- branches/views.py
+++ branches/views.py
@@ -1,9 +1,8 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import Branch
-from .serializers import ScheduleSerializer, BranchSerializer
+from .serializers import BranchSerializer
 
 
 class BranchViewSet(viewsets.ModelViewSet):
     queryset = Branch.objects.all()
     serializer_class = BranchSerializer
-    # permission_classes = [permissions.IsAdminUser]
