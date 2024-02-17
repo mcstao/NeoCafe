84b4eecb90e85c9151da6f188cd1d9b6382e8149
AibekYrysbekov <yrysbekovayba@gmail.com>
2024-02-17 13:54:11 +0600

    updated storage/urls file

--- Neocafe24/urls.py
+++ Neocafe24/urls.py
@@ -21,6 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('branches/', include('branches.urls')),
+    path('storage/', include('storage.urls')),
     path('users/', include('users.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
