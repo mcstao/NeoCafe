5f4aa350aeb7497ba4e4d73632b82459c63e148a
AibekYrysbekov <yrysbekovayba@gmail.com>
2024-03-02 15:17:38 +0600

    updated settings file

--- Neocafe24/settings.py
+++ Neocafe24/settings.py
@@ -94,6 +94,24 @@ WSGI_APPLICATION = 'Neocafe24.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # DATABASES = {
 #     'default': {
