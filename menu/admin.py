03eae90ccd203565b8eed15e959309f838030cd1
mcstao <arislg99@gmail.com>
2024-03-20 05:46:37 +0600

    T3-276 updated orders

--- menu/admin.py
+++ menu/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from menu.models import Menu, Category, ExtraItem, Ingredient
+from menu.models import Menu, Category, ExtraItem, Ingredient, MenuItemIngredient
 
 admin.site.register(Menu)
 admin.site.register(Category)
 admin.site.register(ExtraItem)
 admin.site.register(Ingredient)
+admin.site.register(MenuItemIngredient)
