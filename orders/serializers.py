6b9da3cb1519b3b321cbcbb009c7e5a45cfd43c9
mcstao <arislg99@gmail.com>
2024-03-29 08:09:00 +0600

    T3-276 updated orders extra products

--- orders/serializers.py
+++ orders/serializers.py
@@ -27,7 +27,7 @@ class ExtraProductSerializer(serializers.ModelSerializer):
 class OrderStaffItemSerializer(serializers.ModelSerializer):
     menu_detail = serializers.SerializerMethodField(read_only=True)
     menu_id = serializers.IntegerField()
-    extra_product = ExtraProductSerializer(source='extraitem_set', many=True, read_only=True)
+    extra_product = serializers.SerializerMethodField()
 
     class Meta:
         model = OrderItem
