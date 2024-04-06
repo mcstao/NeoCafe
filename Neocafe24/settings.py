13d58e0b1f8e469c9eb50c1fc54a85cd661149f6
mcstao <arislg99@gmail.com>
2024-04-06 15:47:18 +0600

    T3-194 created client notifications

--- Neocafe24/settings.py
+++ Neocafe24/settings.py
@@ -220,7 +220,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=50),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
