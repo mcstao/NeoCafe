1be6f7a7415f4025f75528800b5972d340e15aa8
mcstao <arislg99@gmail.com>
2024-04-15 22:21:33 +0600

    T3-194 updated nofications list

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import NotificationDeleteView, NotificationAllDeleteView
+from .views import NotificationDeleteView, NotificationAllDeleteView, UserNotificationListView
 
 urlpatterns = [
     path('delete/<int:pk>/', NotificationDeleteView.as_view(), name='delete'),
-    path('delete/all/', NotificationAllDeleteView.as_view(), name='delete-all')
+    path('delete/all/', NotificationAllDeleteView.as_view(), name='delete-all'),
+    path('notifications/', UserNotificationListView.as_view(), name='notifications')
 ]
